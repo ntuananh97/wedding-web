e21258ec7f060450ecb169a625a0cbf8a07eab63
tuananh <anh.nguyentuan@euroland.com>
2025-12-01 15:01:27 +0700

    feat: update calendar

---
+++ src/app/components/Calendar.tsx
@@ -0,0 +1,244 @@
+import React from 'react'
+
+const Calendar = () => {
+  return (
+    <div id="SECTION4" className="ladi-section">
+    <div className="ladi-section-background ladi-lazyload" />
+    <div className="ladi-container">
+      <div id="BOX8" className="ladi-element">
+        <div className="ladi-box ladi-transition ladi-lazyload" />
+      </div>
+      <div id="HEADLINE29" className="ladi-element ladi-animation-hidden">
+        <h3 className="ladi-headline ladi-lazyload">
+          December
+          <br />
+        </h3>
+      </div>
+      <div id="HEADLINE31" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          1<br />
+        </p>
+      </div>
+      <div id="HEADLINE32" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          2<br />
+        </p>
+      </div>
+      <div id="HEADLINE33" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          3<br />
+        </p>
+      </div>
+      <div id="HEADLINE34" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          4<br />
+        </p>
+      </div>
+      <div id="HEADLINE35" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          5<br />
+        </p>
+      </div>
+      <div id="HEADLINE36" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          6<br />
+        </p>
+      </div>
+      <div id="HEADLINE37" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          7<br />
+        </p>
+      </div>
+      <div id="HEADLINE38" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          8<br />
+        </p>
+      </div>
+      <div id="HEADLINE39" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          9<br />
+        </p>
+      </div>
+      <div id="HEADLINE40" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          10
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE41" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          11
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE42" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          12
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE43" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          13
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE44" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          14
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE45" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          15
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE47" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          31
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE48" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          mon
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE49" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          tue
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE50" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">wed</p>
+      </div>
+      <div id="HEADLINE51" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          thur
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE52" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          fri
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE53" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          sat
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE54" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          sun
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE55" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          24
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE56" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          25
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE57" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          26
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE58" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          27
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE59" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          28
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE60" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          29
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE61" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          30
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE62" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          17
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE63" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          18
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE64" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          19
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE65" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          20
+          <br />
+        </p>
+      </div>
+      <div id="IMAGE11" className="ladi-element ladi-animation-hidden">
+        <div className="ladi-image">
+          <div className="ladi-image-background ladi-lazyload" />
+        </div>
+      </div>
+      <div id="HEADLINE66" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          21
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE67" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          22
+          <br />
+        </p>
+      </div>
+      <div id="HEADLINE68" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          23
+          <br />
+        </p>
+      </div>
+    
+      <div id="HEADLINE46" className="ladi-element">
+        <p className="ladi-headline ladi-transition ladi-lazyload">
+          16
+          <br />
+        </p>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Calendar
