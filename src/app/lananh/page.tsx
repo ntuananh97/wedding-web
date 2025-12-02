407e5f74bc7762bdd760c14b11c3f18acc93f132
Anh Nguyen Tuan <ntuananh2897@gmail.com>
2025-12-03 00:04:42 +0700

    feat: commit

---
+++ src/app/lananh/page.tsx
@@ -0,0 +1,13 @@
+import ViewPage from "../components/ViewPage";
+
+const page = () => {
+  return (
+    <ViewPage
+      place="Tư gia nhà gái"
+      address="216 Nguyễn Lương Bằng, TT. Thanh Miện, Thanh Miện, Hải Phòng"
+      mapUrl="https://maps.app.goo.gl/cH9x4wzsDyD5jVocA"
+    />
+  );
+};
+
+export default page;
---
+++ src/app/tuananh/page.tsx
@@ -0,0 +1,13 @@
+import ViewPage from "../components/ViewPage";
+
+const page = () => {
+  return (
+    <ViewPage
+      place="Tư gia nhà trai"
+      address="Trường Tiểu học Ngũ Hùng, Ngũ Hùng, Thanh Miện, Hải Phòng"
+      mapUrl="https://maps.app.goo.gl/EzSR13VdnS4ccP3S6"
+    />
+  );
+};
+
+export default page;
